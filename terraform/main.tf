b1d9f0345e3e66406276dac7657aae0e91b3c527
Ciara Kerckhove <kerckhoveciara@gmail.com>
2021-04-30 01:29:22 +0000

    Adding Jenkins to the terraform script and adding initial pass at the ansible script

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,7 +96,20 @@ resource "aws_instance" "web" {
         "sudo apt-add-repository universe",
         "sudo apt-get update -y",
         "sudo apt-get install git -y",
-        "sudo apt-get install maven -y"
+        "sudo apt-get install maven -y",
+        "wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
+        "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
+        "sudo apt update -qq",
+        "sudo apt install -y default-jre",
+        "sudo apt install -y jenkins",
+        "sudo systemctl start jenkins",
+        "sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080",
+        "sudo sh -c \"iptables-save > /etc/iptables.rules\"",
+        "echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections",
+        "echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections",
+        "sudo apt-get -y install iptables-persistent",
+        "sudo ufw allow 8080",
+        "sudo apt install ansible -y"
     ]    
   }
 
