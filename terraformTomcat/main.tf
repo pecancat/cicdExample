93f41a5709f77a41338e20f77e8f3d4360c63a4a
Ciara Kerckhove <kerckhoveciara@gmail.com>
2021-04-30 15:05:04 +0000

    messing with terraform

--- terraformTomcat/main.tf
+++ terraformTomcat/main.tf
@@ -12,7 +12,7 @@ resource "tls_private_key" "private-key" {
 
 
 resource "aws_key_pair" "deployer" {
-  key_name   = "${terraform.workspace}.deployer-key"
+  key_name   = "${terraform.workspace}.1.deployer-key"
   public_key = tls_private_key.private-key.public_key_openssh
 }
 
@@ -33,54 +33,13 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-
-resource "aws_security_group" "student-sg" {
-  name = "Hello-World-SG"
-  description = "Student security group"
-
-  tags = {
-    Name = "Hello-World-SG"
-    Environment = terraform.workspace
-  }
-}
-
-resource "aws_security_group_rule" "create-sgr-ssh" {
-  security_group_id = aws_security_group.student-sg.id
-  cidr_blocks         = ["0.0.0.0/0"]
-  from_port         = 22
-  protocol          = "tcp"
-  to_port           = 22
-  type              = "ingress"
-  self              = false
-}
-
-resource "aws_security_group_rule" "create-sgr-jenkins" {
-  security_group_id = aws_security_group.student-sg.id
-  cidr_blocks         = ["0.0.0.0/0"]
-  from_port         = 8080
-  protocol          = "tcp"
-  to_port           = 8080
-  type              = "ingress"
-  self              = false
-}
-
-resource "aws_security_group_rule" "create-sgr-outbound" {
-  security_group_id = aws_security_group.student-sg.id
-  cidr_blocks         = ["0.0.0.0/0"]
-  from_port         = 0
-  protocol          = "all"
-  to_port           = 65535
-  type              = "egress"
-  self              = false
-}
-
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer.key_name
   security_groups = ["Hello-World-SG"]
   tags = {
-    Name = "HelloWorld"
+    Name = "Tomcat"
   }
  
   provisioner "remote-exec" {
@@ -94,17 +53,18 @@ resource "aws_instance" "web" {
     inline = [
         "sudo apt-get install software-properties-common",
         "sudo apt-add-repository universe",
+        "sudo apt-add-repository ppa:ansible/ansible -y",
         "sudo apt-get update -y",
         "sudo apt-get install git -y",
         "sudo apt-get install maven -y",
-        "sudo apt-get install tomcat9 -y"
+        "sudo apt-get install tomcat9 -y",
         "sudo ufw allow 8080",
-        "sudo apt install ansible -y"
+        "sudo apt-get install ansible -y"
     ]    
   }
 
   provisioner "local-exec" {
-    command = "terraform output private-key | sed '1d' | sed '28d' | sed '28d' > ~/.ssh/student.pem; chmod 600 ~/.ssh/student.pem"
+    command = "terraform output private-key | sed '1d' | sed '28d' | sed '28d' > ~/.ssh/tomcat.pem; chmod 600 ~/.ssh/tomcat.pem"
   }
 }
 
